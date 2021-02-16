8b708159c84da9f7124b3446255f9dd51611462c
dhrey <d.adeleke@semicolon.africa>
2021-02-16 03:57:38 +0100

    Adding mor feature to the project

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -3,3 +3,14 @@ from gtts import gTTS
 from playsound import playsound
 
 """Convert your Text into Voice with Python and Google APIs"""
+
+# initializing the window
+root = Tk()
+root.geometry("350x300")
+root.configure(bg='ghost white')
+root.title("TEXT TO SPEECH")
+
+label(root, text = "TEXT_TO_SPEECH", font = "arial 20 bold", bg='white smoke').pack()
+label(text)
+
+root.mainloop()
