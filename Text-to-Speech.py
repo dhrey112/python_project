ec290cbfa7055cf450223fac1e27d88e2527279a
dhrey <d.adeleke@semicolon.africa>
2021-02-16 04:02:51 +0100

    Adding more feature to the project

--- Text-to-Speech.py
+++ Text-to-Speech.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from gtts import gTTS
+#from gtts import gTTS
 from playsound import playsound
 
 """Convert your Text into Voice with Python and Google APIs"""
@@ -11,6 +11,12 @@ root.configure(bg='ghost white')
 root.title("TEXT TO SPEECH")
 
 label(root, text = "TEXT_TO_SPEECH", font = "arial 20 bold", bg='white smoke').pack()
-label(text)
+label(text="Dhrey", font = 'arial 15 bold', bg ='white smoke' , width = '20').pack(side = 'bottom')
+
+msg = StringVar()
+Label(root,text ="Enter Text", font = 'arial 15 bold', bg ='white smoke').place(x=20,y=60)
+
+entry_field = Entry(root, textvariable = msg, width='50')
+entry_field.place(x=20, y=100)
 
 root.mainloop()
