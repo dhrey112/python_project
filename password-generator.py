815672125f5d138bdcde5120a20f7cd10f10b255
dhrey <d.adeleke@semicolon.africa>
2021-02-26 11:44:14 +0100

    Adding functionalities to password-generator project

--- password-generator.py
+++ password-generator.py
@@ -1,11 +1,18 @@
 from tkinter import *
 import random, string
-import pyperclip
+
+# import pyperclip
 
 root = Tk()
-root.geometry("400X400")
+root.geometry("400x400")
 root.resizable(0, 0)
 root.title("Password Generator")
 
+Label(root, text='PASSWORD GENERATOR', font='arial 10 bold').pack()
+# Label(root, text='', font='arial 15 bold').pack(side=BOTTOM)
+
+pass_label = Label(root, text='PASSWORD LENGTH', font='arial 8 bold 10').pack()
+pass_len = IntVar()
+length = Spinbox(root, from_ = 8, to_ = 32, textvariable=pass_len, width = 15).pack()
 
 root.mainloop()
