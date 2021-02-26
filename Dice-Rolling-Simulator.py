7729e50dd20571346085a2f2da002cd447c3ab65
dhrey <d.adeleke@semicolon.africa>
2021-02-26 18:48:38 +0100

    Dice Rolling Simulator project, adding functionalities

--- Dice-Rolling-Simulator.py
+++ Dice-Rolling-Simulator.py
@@ -20,15 +20,29 @@ dice = ['die1.png', 'die2.png', 'die3.png',
 # simulating the dice with random numbers between
 # 0 to 6 and generating image
 
-dice_image = ImageTk.PhotoImage(Image.open(random.choices(dice)))
+dice_image = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 
 # Constructing a label for image, adding a button and assigning functionality
 
 # construct a label widget for image
 image_label = tkinter.Label(window, image=dice_image)
 image_label.image = dice_image
-image_label.pack(expand=True) # packing a widget in the parent widget
+image_label.pack(expand=True)  # packing a widget in the parent widget
 
 
+# function activated by button
+def rolling_dice():
+    dice_image = ImageTk.PhotoImage(Image.open(random.choice(dice)))
+    # update image
+    image_label.configure(image=dice_image)
+    # keep a reference
+    image_label.image = dice_image
+
+
+# adding button, and command will use rolling_dice function
+button = tkinter.Button(window, text='Roll the Dice', fg='blue', command=rolling_dice)
+
+# pack a widget in the parent widget
+button.pack(expand=True)
 
 window.mainloop()
