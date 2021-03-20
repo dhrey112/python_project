e31b1542b2776f7eaab535ba93bccc62a823e99d
dhrey <d.adeleke@semicolon.africa>
2021-03-20 15:56:21 +0100

    Currency converter app - defined the convert function

--- currency-converter.py
+++ currency-converter.py
@@ -7,4 +7,19 @@ import tkinter as tk
 # Create the currency Convert class
 
 class currency_converter():
-    def __init__(self, url)
+    def __init__(self, url):
+        self.data = requests.get(url).json()
+        self.currencies = self.data['rate']
+
+
+def convert(self, from_currency, to_currency, amount):
+    initial_amount = amount
+    # first convert it into USD if it is not in USD.
+    # because our base currency is USD
+    if from_currency != 'USD':
+        amount = amount / self.currencies[to_currency]
+
+    # limiting the precision to 4 decimal places
+    amount = round(amount * self.currencies[to_currency], 4)
+    return amount
+
