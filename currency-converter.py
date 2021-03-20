919530da8a7126e3eb2e2fa172d8d869a28aff49
dhrey <d.adeleke@semicolon.africa>
2021-03-20 15:49:07 +0100

    Currency converter app - creating the class

---
+++ currency-converter.py
@@ -0,0 +1,10 @@
+import requests
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+
+# Create the currency Convert class
+
+class currency_converter():
+    def __init__(self, url)
