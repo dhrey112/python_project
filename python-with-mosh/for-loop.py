a66265d81f0f537ee7c61b602bcb1d5b5cb6b9d3
dhrey <d.adeleke@semicolon.africa>
2021-04-04 19:11:45 +0100

    python with mosh

---
+++ python-with-mosh/for-loop.py
@@ -0,0 +1,15 @@
+for item in "Python":
+    print(item)
+
+for item in ["5", 'Mosh', 'Ade']:
+    print(item)
+
+for item in range(3, 30, 4):
+    print(item)
+
+
+prices = [10,20, 30]
+total = 0
+for price in prices:
+    total = total + price
+print(f"Total: {total}")
