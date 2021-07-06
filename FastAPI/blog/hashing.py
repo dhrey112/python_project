15adf6e7ab5374ba79b9c81a60ddc40763a1850b
dhrey <d.adeleke@semicolon.africa>
2021-07-06 17:41:05 +0100

    token

--- FastAPI/blog/hashing.py
+++ FastAPI/blog/hashing.py
@@ -8,3 +8,6 @@ class Hash():
 
     def bcrypt(self):
         return pwd_cxt.hash(self.password)
+
+    def verify(hashed_password, plain_password):
+        return pwd_cxt.verify(plain_password, hashed_password)
--- FastAPI/blog/routers/authentication.py
+++ FastAPI/blog/routers/authentication.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
-from .. import schemas, models, database
+from .. import schemas, models, database, token
+from ..hashing import Hash
+
 router = APIRouter(
     prefix="/login",
     tags=['Authentication']
@@ -9,6 +11,15 @@ router = APIRouter(
 
 @router.post('/')
 def login(request: schemas.Login, db: Session = Depends(database.get_db)):
-    user = db.query(models.)
-    return login
+    user = db.query(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid User credential")
+    
+    if not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid User password")
+
+    # generate a jwt token
+    access_token = token.create_access_token(data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
     
